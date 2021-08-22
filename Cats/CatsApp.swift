cb40a90054b4e0ffb802c7de9a8053a3bf707b4a
Kamil Suleymanov <krakatoor@mail.ru>
2021-08-22 22:32:32 +0300

    Initial Commit

---
+++ Cats/CatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsApp.swift
+//  Cats
+//
+//  Created by Камиль Сулейманов on 22.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct CatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
